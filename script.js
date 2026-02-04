f2effa2b1a0f54ea8c161275663dd20f2e4e30dc
Elizab._.th <72495673+galambova@users.noreply.github.com>
2026-02-04 17:25:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Si se uklikl, ne?",
+    "Šak mooore, nedělej",
+    "Hej ale už",
+    "Nooootáák...",
+    "Yáááááy, Marženko, nedělej!",
+    "Ještě ti dám jednu šanci si to promyslet!",
+    "Dobře, tak ještě jednu",
+    "Snad nechceš, abych byla smutná",
+    "Fááááájn, dobře, už se ptát nebudu",
+    "Ale nottááák, katzé! ❤️ (dostaneš knee, jestli dáš ještě ne xd)"
 ];
 
 let messageIndex = 0;
